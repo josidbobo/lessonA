bed5b3760b6c047494e9b2797a12d49887be8bcf
Joshua <josidbobo>
2022-06-20 22:25:08 +0100

    updated tests

--- test/Token.js
+++ test/Token.js
@@ -1,19 +1,41 @@
 const { expect } = require("chai");
 
-describe("Transactions", function () {
-  it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
+describe("Token Contract", function () {
+  beforeEach( async function () {
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("Token");
 
-    const hardhatToken = await Token.deploy();
+    hardhatToken = await Token.deploy();
+    await hardhatToken.deployed();
 
-    // Transfer 50 tokens from owner to addr1
+  });
+  describe("Transactions", function () {
+    beforeEach(async function (){
+       // Transfer 50 tokens from owner to addr1
     await hardhatToken.transfer(addr1.address, 50);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
 
+    });
+  it("Should transfer tokens from owner to another account", async function () {
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+  });
+
+  it("Should decrement the owner balance after transferring", async function (){
+    // Deduct the transferred amount from owner
+    expect(await hardhatToken.balanceOf(owner.address)).to.equal(999950);
+  });
+
+  it("Should transfer tokens from one address to another", async function (){
     // Transfer 50 tokens from addr1 to addr2
     await hardhatToken.connect(addr1).transfer(addr2.address, 50);
     expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
   });
+});
+describe("Balances", function (){
+  it("Should return zero for an account without tokens", async function (){
+    expect(await hardhatToken.balanceOf(addr3.address)).to.be.equal(0);
+  });
+
+});
+
 });
